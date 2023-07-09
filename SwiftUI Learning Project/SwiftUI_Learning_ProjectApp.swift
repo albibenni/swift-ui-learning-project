4ebc3dc063f19fd5ee2b6e72be109d842d7f5382
Alberto Benatti <albi.benni8@gmail.com>
2023-07-09 15:42:24 +0200

    Initial Commit

---
+++ SwiftUI Learning Project/SwiftUI_Learning_ProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_Learning_ProjectApp.swift
+//  SwiftUI Learning Project
+//
+//  Created by Alberto Benatti on 09/07/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_Learning_ProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUI Learning ProjectUITests/SwiftUI_Learning_ProjectUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwiftUI_Learning_ProjectUITestsLaunchTests.swift
+//  SwiftUI Learning ProjectUITests
+//
+//  Created by Alberto Benatti on 09/07/23.
+//
+
+import XCTest
+
+final class SwiftUI_Learning_ProjectUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
